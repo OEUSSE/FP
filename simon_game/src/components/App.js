cc5aad093263da95c64155289fe7e2bd454f9647
Óscar Eusse Flórez <scr.eusse@gmail.com>
2018-05-15 15:07:09 -0500

    [Add] Styles

--- simon_game/src/components/App.js
+++ simon_game/src/components/App.js
@@ -5,7 +5,30 @@ class App extends Component {
   render() {
     return (
       <div className="simon-game-app">
-
+        <div className="controls">
+          <h1>Simon</h1>
+          <div className="opts-set-a">
+            <div className="count-output">- -</div>
+            <button className="btn-start btn"></button>
+            <button className="btn-strict btn"></button>
+          </div>
+          <div className="labels">
+            <span>count</span>
+            <span>start</span>
+            <span>strict</span>
+          </div>
+          <div className="opts-power">
+            <input id="power" type="checkbox" />
+          </div>
+        </div>
+        <div className="top">
+          <div className="press-button green"></div>
+          <div className="press-button red"></div>
+        </div>
+        <div className="bottom">
+          <div className="press-button yellow"></div>
+          <div className="press-button blue"></div>
+        </div>
       </div>
     );
   }
